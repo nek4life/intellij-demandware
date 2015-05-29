9dd6268e5da0c0fa057e93e608cef7331c189cd7
Charlie Choiniere <nek4life@gmail.com>
2015-05-28 22:07:38 -0400

    Added Demandware Icons

--- src/com/demandware/studio/projectWizard/DWModuleType.java
+++ src/com/demandware/studio/projectWizard/DWModuleType.java
@@ -3,6 +3,7 @@ package com.demandware.studio.projectWizard;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.module.ModuleTypeManager;
+import com.intellij.openapi.util.IconLoader;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -38,11 +39,11 @@ public class DWModuleType extends ModuleType<DWModuleBuilder> {
 
     @Override
     public Icon getBigIcon() {
-        return AllIcons.General.Information;
+        return IconLoader.getIcon("/icons/demandware.png");
     }
 
     @Override
     public Icon getNodeIcon(@Deprecated boolean isOpened) {
-        return AllIcons.General.Information;
+        return IconLoader.getIcon("/icons/demandware.png");
     }
 }
