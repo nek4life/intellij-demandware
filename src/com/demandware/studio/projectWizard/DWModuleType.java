8e4907538d081eb605833380a2be1bedacdb0085
Charlie Choiniere <nek4life@gmail.com>
2015-06-01 13:12:21 -0400

    Changed Module Type ID

--- src/com/demandware/studio/projectWizard/DWModuleType.java
+++ src/com/demandware/studio/projectWizard/DWModuleType.java
@@ -1,6 +1,5 @@
 package com.demandware.studio.projectWizard;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.module.ModuleTypeManager;
 import com.intellij.openapi.util.IconLoader;
@@ -9,7 +8,7 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 
 public class DWModuleType extends ModuleType<DWModuleBuilder> {
-    private static final String ID = "DW_MODULE_TYPE";
+    private static final String ID = "DWModuleType";
 
     public DWModuleType() {
         super(ID);
