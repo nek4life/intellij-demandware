62a5bc79ed00b1321ec380973a418cb5ad54fbdf
Charlie Choiniere <nek4life@gmail.com>
2015-07-13 08:26:04 -0400

    Added DS Icon support if DS is a registered file type

---
+++ src/com/demandware/studio/ds/DSIconProvider.java
@@ -0,0 +1,23 @@
+package com.demandware.studio.ds;
+
+import com.demandware.studio.AllIcons;
+import com.intellij.ide.IconProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class DSIconProvider extends IconProvider {
+    @Nullable
+    @Override
+    public Icon getIcon(PsiElement element, int flags) {
+        PsiFile containingFile = element.getContainingFile();
+        if (containingFile != null) {
+            if (containingFile.getName() != null && containingFile.getName().contains(".ds")) {
+                return AllIcons.DW_DS_ICON;
+            }
+        }
+        return null;
+    }
+}
