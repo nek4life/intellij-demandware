9595af571bcc880570a9930a3cdcbc7bc1e680e3
Charlie Choiniere <nek4life@gmail.com>
2015-07-13 08:36:00 -0400

    Attempt at registering ds as JavaScript

--- src/com/demandware/studio/ds/DSLanguageTypeFactory.java
+++ src/com/demandware/studio/ds/DSLanguageTypeFactory.java
@@ -1,5 +1,6 @@
 package com.demandware.studio.ds;
 
+import com.intellij.lang.javascript.JavaScriptFileType;
 import com.intellij.openapi.fileTypes.FileTypeConsumer;
 import com.intellij.openapi.fileTypes.FileTypeFactory;
 import org.jetbrains.annotations.NotNull;
@@ -7,6 +8,6 @@ import org.jetbrains.annotations.NotNull;
 public class DSLanguageTypeFactory extends FileTypeFactory {
     @Override
     public void createFileTypes(@NotNull FileTypeConsumer consumer) {
-        consumer.consume(DSFileType.INSTANCE, "ds");
+        consumer.consume(JavaScriptFileType.INSTANCE, "ds");
     }
 }
